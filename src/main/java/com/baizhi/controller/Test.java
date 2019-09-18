e7524693c268e8eb0438a23df9adde90dc418964
guoyuangyf <55435354+guoyuangyf@users.noreply.github.com>
2019-09-18 16:25:07 +0800

    Update Test.java

--- src/main/java/com/baizhi/controller/Test.java
+++ src/main/java/com/baizhi/controller/Test.java
@@ -5,6 +5,6 @@ public class Test {
         String s1 = "hello";
         String s2 = "he" + new String("llo");
         System.out.println(s1 == s2);
-
+        System.out.println(s1 == s2);
     }
 }
